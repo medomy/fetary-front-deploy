0335a32346a78111f2c42b992643b8bf380e1bcb
Mohamed Mobarak <mohamed.mobarak@act.eg>
2022-11-16 11:09:15 +0200

    mmm

--- src/features/details/components/details_side/details_side.js
+++ src/features/details/components/details_side/details_side.js
@@ -7,6 +7,8 @@ function DetailsSide({ item }) {
     const dispatch = useDispatch();
     const { t } = useTranslation();
     const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+    const lang = useSelector((state)=> state.lang.lang);
+    const isDark = useSelector((state)=> state.theme.isDarkMode);
     const addToCart = (item) => {
         if (isLoggedIn) {
             dispatch(addItems(item));
@@ -15,20 +17,23 @@ function DetailsSide({ item }) {
         else alert("you are not registered yet");
     }
     return (
-        <section className={styles.details_side}>
+        <section className={`${styles.details_side}`}>
+            <div className='row justify-content-center mb-3'>
+                <h2 className={`${isDark ? 'dark-mode-txt':''}`}>{lang === 'ar' ? item.name_ar : item.name_en}</h2>
+            </div>
             <div className='row justify-content-between mb-3'>
                 <div className='col-md-5'>
-                    <span className={styles.price_space}>
+                    <span className={`${styles.price_space}`}>
                         <i className="bi bi-currency-pound me-1"></i>
                         <span>{item.price}</span>
                     </span>
                 </div>
                 <div className='col-md-5 d-flex justify-content-end'>
-                    <i className={`bi bi-heart ${styles.heart_icon}`}></i>
+                    <i className={`bi bi-heart ${styles.heart_icon} ${isDark ? 'dark-mode-txt':''}`}></i>
                 </div>
             </div>
             <div className={styles.description_part}>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nisl tortor, lobortis non tortor sit amet, iaculis rhoncus ipsum. Fusce ornare nunc maximus dui molestie.</p>
+                <p className={`${isDark ? 'dark-mode-txt':''}`}>{lang === 'ar' ? item.description_ar : item.description_en}</p>
             </div>
             <div className={`${styles.counter_div} mt-4`}>
                 <span className={styles.counter_btn}>
